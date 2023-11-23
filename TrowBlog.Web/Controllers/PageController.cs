28ecaab3ae358900ea7eda7d27f5d5815d7dcfff
Travers Polius <traverse.polius@outlook.com>
2023-11-22 23:01:18 -0800

    Dashboard added

---
+++ TrowBlog.Web/Controllers/PageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrowBlog.Web.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult About() { return View(); }
+
+        public IActionResult Contact() { return View(); }
+
+        public IActionResult PrivacyPolicy() { return View(); }
+    }
+}
--- TrowBlog.Web/Program.cs
+++ TrowBlog.Web/Program.cs
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
