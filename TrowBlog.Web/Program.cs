aeb07d6e687a59247114f4762c0041de015533e3
Travers Polius <traverse.polius@outlook.com>
2023-11-26 22:42:31 -0800

    Admin dashboard added

--- TrowBlog.Web/Program.cs
+++ TrowBlog.Web/Program.cs
@@ -40,6 +40,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
 
 void DataSeeding()
