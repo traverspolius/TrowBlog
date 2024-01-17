4c1208270bd18f8280f729ea823d72f66d6d2034
Travers Polius <64287688+traverspolius@users.noreply.github.com>
2024-01-16 20:43:21 -0800

    ThumbnailUrl field added to the Post model.

--- TrowBlog.Web/Areas/Admin/Controllers/PostController.cs
+++ TrowBlog.Web/Areas/Admin/Controllers/PostController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TrowBlog.Web.Areas.Admin.Controllers
 {
 	[Area("Admin")]
+	[Authorize]
 	public class PostController : Controller
 	{
 		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Create()
 		{
 			return View();
 		}
